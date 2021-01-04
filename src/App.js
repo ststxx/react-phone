023c7eb79acfe2606dc1d51e81e966df1ce81abe
st <deanxx@naver.com>
2021-01-04 21:27:16 +0900

    react phone book

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react';
+import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  id = 3;
+
+  state = {
+    information: [
+      {
+        id: 0,
+        name: 'name0',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: 'name1',
+        phone: '010-0000-0001'
+      },
+      {
+        id: 2,
+        name: 'name2',
+        phone: '010-0000-0002'
+      },
+    ],
+    keyword: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value,
+    })
+  }
+
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({
+        ...data,
+        id: this.id++,
+      })
+    });
+  }
+
+  handleRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    });
+  }
+
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info => {
+          if (info.id === id) {
+            return {
+              id,
+              ...data,
+            };
+          }
+          return info;
+        }
+      )
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <PhoneForm onCreate={this.handleCreate} />
+        <input 
+          value={this.state.keyword}
+          onChange={this.handleChange}
+          placeholder="검색..."
+        />
+        <PhoneInfoList 
+          data={this.state.information.filter(
+            info => info.name.indexOf(this.state.keyword) > -1
+          )}
+          onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
